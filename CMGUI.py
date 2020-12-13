41ea68240a0f836ced7e087b6e027acb3592bd05
seanc <seanchiang226@gmail.com>
2020-12-13 02:39:55 -0500

    Text editor and save/load added

--- CMGUI.py
+++ CMGUI.py
@@ -4,19 +4,97 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
 from kivy.uix.scrollview import ScrollView
-from kivy.properties import (ListProperty, StringProperty)
+from kivy.properties import (ListProperty, StringProperty, ObjectProperty)
 from string import ascii_lowercase as lower
 
 import countermachine_david as cm
+import os
 
 COLOR = [0, 0, 1, 1]
 HIGHLIGHT = [1, 1, 0, 1]
 TRANSPARENT = [0, 0, 0, 0]
 
 class MainWindow(Widget):
-    pass
+    # Draws flowchart
+
+    wrapper = ScrollView()
+
+    def draw_flowchart(self, filename):
+
+        self.wrapper = ScrollView(do_scroll_y=True,
+                             id='drawing')
+
+        assembled = cm.assemble_from_file(filename)[0]
+        print(assembled)
+        components = diagram(assembled)
+
+        # Assign the number of column, spacing and padding
+        root = GridLayout(size_hint_y=None, cols=3, padding=25, spacing=3, row_default_height='40dp',
+                          row_force_default=True)
+        root.bind(minimum_height=root.setter('height'))
+
+        for component in components:
+            if component is None:
+                root.add_widget(Blank())
+                continue
+
+            widget, args, line = component
+
+            component = widget(**args)
+
+            root.add_widget(component)
+
+        self.wrapper.add_widget(root)
+
+        self.ids.flowchart.add_widget(self.wrapper)
+        return
+
+    def clear_flowchart(self):
+        self.ids.flowchart.remove_widget(self.wrapper)
+        return
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class CPEditor(BoxLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def file_chooser(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.4, 0.7))
+        self._popup.open()
+
+    def file_saver(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content, size_hint=(0.4, 0.7))
+        self._popup.open()
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
 
 class Component(Widget):
     line_color = ListProperty(COLOR)
@@ -80,43 +158,13 @@ class Connector(Component):
 
 # Create the App class 
 class CMGUIApp(App):
-
     def __init__(self):
         super().__init__()
 
-    # Draws flowchart
-    def draw_flowchart(self, filename):
-        assembled = cm.assemble_from_file(filename)[0]
-        print(assembled)
-        components = diagram(assembled)
-
-        wrapper = ScrollView(do_scroll_y=True)
-
-        # Assign the number of column, spacing and padding
-        root = GridLayout(size_hint_y=None, cols=3, padding=25, spacing=3, row_default_height='40dp',
-                          row_force_default=True)
-        root.bind(minimum_height=root.setter('height'))
-
-        for component in components:
-            if component is None:
-                root.add_widget(Blank())
-                continue
-
-            widget, args, line = component
-
-            component = widget(**args)
-
-            root.add_widget(component)
-
-        wrapper.add_widget(root)
-        return wrapper
-
     def build(self):
-        gui = MainWindow()
+        #self.gui.ids.flowchart.add_widget(self.draw_flowchart('pow.cp'))
 
-        gui.ids.flowchart.add_widget(self.draw_flowchart('pow.cp'))
-
-        return gui
+        return MainWindow()
 
 def add_attributes_or_create_connector(line, index, attributes):
     connector = line[index]
@@ -264,4 +312,5 @@ if __name__ == '__main__':
     #assembled = cm.assemble_from_file('pow.cp')[0]
     #print(assembled)
     #components = diagram(assembled)
+    Window.size = (1400, 900)
     CMGUIApp().run()
